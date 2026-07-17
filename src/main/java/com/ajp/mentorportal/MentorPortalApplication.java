f554993e1839b6f8f267f2be207cda036e971c47
Aaditya Pratap Singh <aaditya9feb@gmail.com>
2026-07-17 11:59:30 +0530

    feat: Spring Boot foundation - pom.xml, SecurityConfig, application.yml, health actuator

---
+++ src/main/java/com/ajp/mentorportal/MentorPortalApplication.java
@@ -0,0 +1,12 @@
+package com.ajp.mentorportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorPortalApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ajp/mentorportal/MentorPortalApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ajp.mentorportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
